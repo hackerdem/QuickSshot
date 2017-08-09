0638467b81a226f7722f34657a2fb7e20ec0618f
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-08-09 10:30:50 +1000

    first

---
+++ screenshot.py
@@ -0,0 +1,35 @@
+from pynput import keyboard
+import os
+import subprocess,datetime
+import pyscreeze
+
+repository='C:\Screenshot_Repository'
+def numbering():  
+    return len([name for name in os.listdir(repository) if os.path.isfile(os.path.join(repository, name))])+1
+
+def screenshot(shot,scrshot_time):
+    number_item=numbering()
+    shot.save('{}\{}-{}.jpeg'.format(repository,number_item,scrshot_time),'jpeg')
+  
+def repo():
+    
+    
+    if not os.path.isdir(repository):os.makedirs(repository)
+    scrshot_time=datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    shot=pyscreeze.screenshot()
+    screenshot(shot,scrshot_time)
+    
+    
+def on_press(key):
+    try:
+        shot=None
+        if str(key)=="Key.print_screen":
+            repo()
+    except Exception as e:
+        print("error")
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+    
+
+        
+        
---
+++ subpro.py
@@ -0,0 +1,7 @@
+import subprocess
+def main():
+    subprocess.call("python screenshot.py")
+    
+    
+if __name__=="__main__": 
+    main()
